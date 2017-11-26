91959919096805534705e55fb5315e56d9dd288b
Iku Iwasa <iku.iwasa@gmail.com>
2017-11-27 00:34:39 +0900

    Implement delete message with specified channel and ts

--- src/main.rs
+++ src/main.rs
@@ -7,23 +7,33 @@ use std::process::exit;
 use dotenv::dotenv;
 use slack_api::requests::{Client, Error};
 use slack_api::channels::ListError;
+use slack_api::chat::{DeleteError, DeleteRequest};
 
 fn main() {
     dotenv().ok();
 
     let mut args = env::args();
-    if let Some(channel) = args.nth(1) {
-        println!("{}", channel);
-        let token = env::var("SLACK_API_TOKEN").expect("SLACK_API_TOKEN is not set.");
-        let client = slack_api::requests::default_client().unwrap();
-
-        let id = find_channel_id(&client, &token, &channel);
-        println!("{:?}", id);
-    } else {
-        eprintln!("usage: slack-delex CHANNEL_NAME");
+    let arg1 = args.nth(1);
+    let arg2 = args.next();
+    if arg1.is_none() || arg2.is_none() {
+        eprintln!("usage: slack-delex CHANNEL_NAME TS");
         exit(1);
     }
+    let channel_name = arg1.unwrap();
+    let ts = arg2.unwrap();
 
+    let token = env::var("SLACK_API_TOKEN").expect("SLACK_API_TOKEN is not set.");
+    let client = slack_api::requests::default_client().unwrap();
+
+    match find_channel_id(&client, &token, &channel_name) {
+        Ok(channel_id) => {
+            match delete_message(&client, &token, &channel_id, &ts) {
+                Ok(_) => println!("Message deleted: {}", ts),
+                Err(err) => eprintln!("Message delete failed: {}", err),
+            }
+        },
+        Err(err) => eprintln!("Channel list failed: {}", err),
+    }
 }
 
 fn find_channel_id(client: &Client, token: &str, name: &str) -> Result<String, ListError<Error>>
@@ -39,4 +49,14 @@ fn find_channel_id(client: &Client, token: &str, name: &str) -> Result<String, L
         }
     }
     Err("Channel not found".into())
+}
+
+fn delete_message(client: &Client, token: &str, channel_id: &str, ts: &str) -> Result<(), DeleteError<Error>> {
+    let request = DeleteRequest {
+        channel: channel_id,
+        ts: ts,
+        as_user: None,
+    };
+    let _ = slack_api::chat::delete(client, token, &request)?;
+    Ok(())
 }
