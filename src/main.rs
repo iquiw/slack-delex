061d23d9bd32c8ddd70b24fc20ad4c1de3c8c40c
Iku Iwasa <iku.iwasa@gmail.com>
2017-12-17 17:58:55 +0900

    Introduce trait to perform Slack operations
    so that dry-run client can implement the trait.

--- src/main.rs
+++ src/main.rs
@@ -13,11 +13,9 @@ use std::thread;
 
 use clap::{Arg, App};
 use dotenv::dotenv;
-use slack_api::requests::{Client, Error};
-use slack_api::channels::ListError;
-use slack_api::chat::{DeleteError, DeleteRequest};
 
 mod json;
+mod slack;
 
 fn main() {
     dotenv().ok();
@@ -50,51 +48,30 @@ fn main() {
     let delay = time::Duration::from_millis(value_t_or_exit!(matches, "delay", u64));
     let json_file = matches.value_of("JSON_FILE").unwrap();
 
-    let token = env::var("SLACK_API_TOKEN").expect("SLACK_API_TOKEN is not set.");
-    let client = slack_api::requests::default_client().unwrap();
+    let client: Box<slack::DelexClient> = if dry_run {
+        Box::new(slack::DryRunClient::new())
+    } else {
+        let token = env::var("SLACK_API_TOKEN").expect("SLACK_API_TOKEN is not set.");
+        Box::new(slack::SlackDelexClient::new(&token))
+    };
 
-    match find_channel_id(&client, &token, &channel_name) {
+    match client.find_channel_id(&channel_name) {
         Ok(channel_id) => {
             println!("Channel: {}", channel_id);
             let msgs = json::read_json(json_file).unwrap();
             for msg in msgs {
                 let ts = msg.ts();
-                if dry_run {
-                    println!("Would delete: {}", ts);
-                } else {
-                    match delete_message(&client, &token, &channel_id, ts) {
-                        Ok(_) => println!("Message deleted: {}", ts),
-                        Err(err) => eprintln!("Message delete failed: {}", err),
-                    }
-                    thread::sleep(delay);
+                match client.delete_message(&channel_id, ts) {
+                    Ok(_) => if dry_run {
+                        println!("Would delete: {}", ts);
+                    } else {
+                        println!("Message deleted: {}", ts);
+                    },
+                    Err(err) => eprintln!("Message delete failed: {}", err),
                 }
+                thread::sleep(delay);
             }
         },
         Err(err) => eprintln!("Channel list failed: {}", err),
     }
-}
-
-fn find_channel_id(client: &Client, token: &str, name: &str) -> Result<String, ListError<Error>>
-{
-    let response = slack_api::channels::list(client, token, &Default::default())?;
-    if let Some(channels) = response.channels {
-        for channel in channels {
-            if let Some(n) = channel.name {
-                if n == name {
-                    return channel.id.ok_or("ID is not avaiable".into());
-                }
-            }
-        }
-    }
-    Err("Channel not found".into())
-}
-
-fn delete_message(client: &Client, token: &str, channel_id: &str, ts: &str) -> Result<(), DeleteError<Error>> {
-    let request = DeleteRequest {
-        channel: channel_id,
-        ts: ts,
-        as_user: None,
-    };
-    let _ = slack_api::chat::delete(client, token, &request)?;
-    Ok(())
 }
---
+++ src/slack.rs
@@ -0,0 +1,67 @@
+use slack_api;
+use slack_api::requests::{Client, Error};
+use slack_api::channels::ListError;
+use slack_api::chat::{DeleteError, DeleteRequest};
+
+pub trait DelexClient {
+    fn find_channel_id(&self, name: &str) -> Result<String, ListError<Error>>;
+    fn delete_message(&self, channel_id: &str, ts: &str) -> Result<(), DeleteError<Error>>;
+}
+
+pub struct SlackDelexClient {
+    client: Client,
+    token: String,
+}
+
+pub struct DryRunClient;
+
+impl SlackDelexClient {
+    pub fn new(token: &str) -> Self {
+        let client = slack_api::requests::default_client().unwrap();
+        SlackDelexClient {
+            client: client,
+            token: token.to_string()
+        }
+    }
+}
+
+impl DelexClient for SlackDelexClient {
+    fn find_channel_id(&self, name: &str) -> Result<String, ListError<Error>>
+    {
+        let response = slack_api::channels::list(&self.client, &self.token, &Default::default())?;
+        if let Some(channels) = response.channels {
+            for channel in channels {
+                if let Some(n) = channel.name {
+                    if n == name {
+                        return channel.id.ok_or("ID is not avaiable".into());
+                    }
+                }
+            }
+        }
+        Err("Channel not found".into())
+    }
+
+    fn delete_message(&self, channel_id: &str, ts: &str) -> Result<(), DeleteError<Error>> {
+        let request = DeleteRequest {
+            channel: channel_id,
+            ts: ts,
+            as_user: None,
+        };
+        let _ = slack_api::chat::delete(&self.client, &self.token, &request)?;
+        Ok(())
+    }
+}
+
+impl DryRunClient {
+    pub fn new() -> Self { DryRunClient }
+}
+
+impl DelexClient for DryRunClient {
+    fn find_channel_id(&self, _name: &str) -> Result<String, ListError<Error>> {
+        Ok("DUMMY_ID".to_string())
+    }
+
+    fn delete_message(&self, _channel_id: &str, _ts: &str) -> Result<(), DeleteError<Error>> {
+        Ok(())
+    }
+}
